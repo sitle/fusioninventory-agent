279acde0193d1df711ba5254e07e457bd17f0db5
Léonard TAVAE <leonard.tavae@informatique.gov.pf>
2015-07-17 14:15:05 -1000

    Rubocop/foodcritic applied.

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,19 +6,18 @@
 #
 # Copyright 2012
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the ''License'');
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# distributed under the License is distributed on an ''AS IS'' BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 
 default['fusioninventory-agent']['conf_dir'] = '/etc/fusioninventory'
 default['fusioninventory-agent']['server_url'] = 'http://www.example.com/glpi/plugins/fusioninventory/'
@@ -31,16 +30,16 @@ default['fusioninventory-agent']['no-wakeonlan'] = '0'
 default['fusioninventory-agent']['no-snmpquery'] = '0'
 default['fusioninventory-agent']['no-netdiscovery'] = '0'
 default['fusioninventory-agent']['no-p2p'] = '0'
-default['fusioninventory-agent']['tag'] = ""
-default['fusioninventory-agent']['proxy'] = ""
-default['fusioninventory-agent']['proxy_user'] = ""
-default['fusioninventory-agent']['proxy_password'] = ""
-default['fusioninventory-agent']['ca-cert-dir'] = ""
-default['fusioninventory-agent']['ca-cert-file'] = ""
+default['fusioninventory-agent']['tag'] = ''
+default['fusioninventory-agent']['proxy'] = ''
+default['fusioninventory-agent']['proxy_user'] = ''
+default['fusioninventory-agent']['proxy_password'] = ''
+default['fusioninventory-agent']['ca-cert-dir'] = ''
+default['fusioninventory-agent']['ca-cert-file'] = ''
 default['fusioninventory-agent']['no-httpd'] = '0'
-default['fusioninventory-agent']['httpd-ip'] = ""
+default['fusioninventory-agent']['httpd-ip'] = ''
 default['fusioninventory-agent']['httpd-port'] = '62354'
-default['fusioninventory-agent']['httpd-trust'] = ""
+default['fusioninventory-agent']['httpd-trust'] = ''
 default['fusioninventory-agent']['daemon'] = '1'
 default['fusioninventory-agent']['lazy'] = '0'
 default['fusioninventory-agent']['mode'] = 'daemon'
--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
-maintainer       "Mat Davies"
-maintainer_email "ashmere@gmail.com"
-license          "http://www.apache.org/licenses/LICENSE-2.0"
-description      "Installs/Configures fusioninventory-agent"
+name 'fusioninventory-agent'
+maintainer 'Mat Davies'
+maintainer_email 'ashmere@gmail.com'
+license 'http://www.apache.org/licenses/LICENSE-2.0'
+description 'Installs/Configures fusioninventory-agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.2"
+version '0.1.2'
 
-depends          "apt"
+depends 'apt'
